e5f09d7bb902f1c1e85cd7df24e27612c847fe24
audy910 <54282839+audy910@users.noreply.github.com>
2025-05-12 13:32:27 -0700

    Add files via upload

---
+++ recognizeTotem.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import time
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import img_to_array
+from picamera2 import Picamera2
+
+# Load the model
+model = load_model("/home/robotics/Downloads/RoboCarV4_6Epochs_NewRed.keras")
+
+# Define labels
+class_labels = ["green", "red", "yellow"]
+
+# Initialize camera
+camera = Picamera2()
+camera.start()
+
+CONFIDENCE_THRESHOLD = 0.95
+
+
+try:
+    while True:
+        frame = camera.capture_array()
+
+        # Resize and preprocess the frame
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+        resized = cv2.resize(frame_rgb, (150, 150))
+
+        image = img_to_array(resized) / 255.0
+        image = np.expand_dims(image, axis=0)
+
+        # Predict
+        prediction = model.predict(image)
+        class_index = np.argmax(prediction[0])
+        confidence = prediction[0][class_index]
+        
+        if confidence > CONFIDENCE_THRESHOLD:
+            label = class_labels[class_index]
+            print(f"Totem Color: {label}")
+
+        # Break on pressing 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+finally:
+    camera.stop()
+    cv2.destroyAllWindows()
