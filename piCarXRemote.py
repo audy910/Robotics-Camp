d4c8cc19e25f6542527238102d150c6fbcb2af45
audy910 <54282839+audy910@users.noreply.github.com>
2025-05-12 13:33:20 -0700

    Add files via upload

--- piCarXRemote.py
+++ piCarXRemote.py
@@ -4,6 +4,7 @@ import time
 from picamera2 import Picamera2
 
 ser = serial.Serial("/dev/rfcomm0", 9600, timeout = 1)
+
 ser.reset_input_buffer()
 
 # init picarx
@@ -12,30 +13,30 @@ px = Picarx()
 camera = Picamera2()
 
 
-count = 267
-speed = 0
+#count = 1
+
+speed = 10
 px.set_dir_servo_angle(7)
 while True:
-    line = ser.readline().decode("utf-8").rstrip()
+    line = ser.read().decode("utf-8")
     print(line)
-    if("speed" in line):
+    if('1' in line):
         speed = 100
         px.set_dir_servo_angle(7)
         px.forward(speed)
         print(line)
-    if("right" in line):
-        px.set_dir_servo_angle(15)
+    elif('2' in line):
+        px.set_dir_servo_angle(30)
         print(line)
-    if("left" in line):
-        px.set_dir_servo_angle(-8)
+    elif('3' in line):
+        px.set_dir_servo_angle(-22)
         print(line)
-    if("slow" in line):
+    elif('4' in line):
         speed = 0
         px.forward(speed)
         print(line)
-    if(speed > 0):
-        camera.start()
-        camera.capture_file('/home/robotics/Desktop/dataset/rpicam_image'+str(count)+'.jpg')
-        count+=1
-        camera.stop()
-        time.sleep(.1)
+#    if(speed > 0):
+#        camera.start()
+#        camera.capture_file('/home/robotics/Desktop/dataset/rpicam_image_red'+str(count)+'.jpg')
+#         count+=1
+#         camera.stop()
