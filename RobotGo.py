36af2c64c306c7bbfdd421944ef8c097ffcb1911
audy910 <54282839+audy910@users.noreply.github.com>
2025-05-14 14:14:12 -0700

    Update RobotGo.py
    with servo and faster connection

--- RobotGo.py
+++ RobotGo.py
@@ -2,10 +2,20 @@ import serial
 from adafruit_motor import motor as Motor
 import time
 import board
+import RPi.GPIO as GPIO
 from adafruit_motorkit import MotorKit
 
 kit=MotorKit(i2c=board.I2C())
 
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17,GPIO.OUT)
+p = GPIO.PWM(17,50)
+p.start(0)
+
+p.ChangeDutyCycle(7)
+p.ChangeDutyCycle(0)
 #serial port for bluetooth
 ser = serial.Serial("/dev/rfcomm0", 9600, timeout = 1)
 ser.reset_input_buffer()
@@ -15,19 +25,27 @@ speedRight =0
 kit.motor1.throttle=speedLeft
 kit.motor2.throttle=speedRight
 while True:
-	line = ser.readline().decode("utf-8").rstrip()
+	line = ser.read().decode("utf-8")
 	print(line)
-	if("speed" in line):
-		speedLeft = 0.75
+	if('1' in line):
+		speedLeft = 0.75 #forward
 		speedRight= 0.75
-	if("right" in line):
-		speedLeft = 0.0
+		p.ChangeDutyCycle(7)
+
+	elif('2' in line):
+		speedLeft = 0.75 #right
 		speedRight = 0.5
-	if("left" in line):
-		speedLeft = 0.5
-		speedRight = 0.0
-	if("slow" in line):
-		speedLeft = 0.2
-		speedRight = 0.2
+		p.ChangeDutyCycle(11)
+	elif('3' in line):
+		speedLeft = 0.5 #left
+		speedRight = 0.75
+		p.ChangeDutyCycle(5)
+	elif('4' in line):
+		speedLeft = 0 #slow
+		speedRight = 0
+		p.ChangeDutyCycle(7)
+
 	kit.motor1.throttle=speedLeft
 	kit.motor2.throttle=speedRight
+p.stop()
+GPIO.cleanup()
