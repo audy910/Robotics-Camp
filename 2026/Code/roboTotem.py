3017d1e1d57cbe1c9698ce86949f34812261227b
Audrey Reinhard <54282839+audy910@users.noreply.github.com>
2026-06-24 16:52:14 -0700

    Add files via upload

--- 2026/Code/roboTotem.py
+++ 2026/Code/roboTotem.py
@@ -6,22 +6,24 @@ from picamera2.encoders import Quality
 from ultralytics import YOLO
 
 # Load the YOLO model once
-model_path="PASTE_FILE_PATH_HERE"
+model_path = "/home/robotics/Downloads/weights.pt"
 model = YOLO(model_path, task='detect')
 labels = model.names
 
 # Define threshold
-confidence_threshold = 0.8
+confidence_threshold = 0.5
 
-# #Initialize camera once
+# Initialize camera once
 camera = Picamera2()
 camera.start()
 
 def viewTotem():
-    global num_frames
     try:
+        
         frame = camera.capture_array()
-        frame = cv2.cvtColor(np.copy(frame), cv2.COLOR_BGRA2BGR)
+        frame = cv2.cvtColor(np.copy(frame), cv2.COLOR_BGRA2RGB)
+
+
         results = model.track(frame, verbose=False)
         detections = results[0].boxes
         confidence = 0
@@ -29,11 +31,15 @@ def viewTotem():
             det = detections[0]
             classidx = int(det.cls.item())
             confidence = det.conf.item()
+
         print(f"confidence: {confidence}")
-        if confidence >=  confidence_threshold:
+        if confidence >= confidence_threshold:
             return labels[classidx]
         return "no"
-        
+
     except Exception as e:
         print("Error in viewTotem():", type(e).__name__)
         return "no"
+
+while True:
+    print(viewTotem())
